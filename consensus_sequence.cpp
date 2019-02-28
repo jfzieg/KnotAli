f8b35fe3ce63bfd523561c750f52394383324a55
jfzieg <jzieg@uvm.edu>
2019-02-28 12:46:21 -0500

    Adding work files.

---
+++ consensus_sequence.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Joseph Zieg on 2/28/19.
+//
+
+#include "consensus_sequence.h"
---
+++ consensus_sequence.h
@@ -0,0 +1,8 @@
+//
+// Created by Joseph Zieg on 2/28/19.
+//
+
+#ifndef HFOLD_CONSENSUS_SEQUENCE_H
+#define HFOLD_CONSENSUS_SEQUENCE_H
+
+#endif //HFOLD_CONSENSUS_SEQUENCE_H
